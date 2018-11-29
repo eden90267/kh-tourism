986acb616582ef68e088108eec6f7cad350831ad
Eden Liu <eden90267@gmail.com>
2018-11-29 11:29:08 +0800

    First commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'bootstrap';
+
 import App from './App.vue';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
+Vue.use(VueAxios, axios);
+Vue.component('Loading', Loading);
 
 new Vue({
   store,
